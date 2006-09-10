1e0bbb36cd2f813a9bbbae5c500a56205195040f
Gerard Vermeulen <gvermeul@users.sourceforge.net>
2006-09-10 10:16:01 +0000

    Improve numpy's backward compatibility by a few aliases.

--- qt3lib/Qwt5/anynumpy.py
+++ qt3lib/Qwt5/anynumpy.py
@@ -6,6 +6,8 @@ for name in ('numpy', 'numarray', 'Numeric'):
          eval(compile('from %s import *' % name, 'eval', 'exec'))
 	 if name == 'numpy':
 	     from numpy.oldnumeric.compat import *
+             Float = float
+             UInt8 = uint8
     except ImportError:
         failed = True
     if not failed:
