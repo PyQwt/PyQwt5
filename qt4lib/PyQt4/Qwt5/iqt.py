1cb4b47d4fc7d78f1cc498e471407554e982ab35
Gerard Vermeulen <gvermeul@users.sourceforge.net>
2007-07-25 17:01:28 +0000

    Implement the idea of Michiel de Hoon to render readline superfluous.

--- qt4lib/PyQt4/Qwt5/iqt.py
+++ qt4lib/PyQt4/Qwt5/iqt.py
@@ -3,27 +3,11 @@
 Provides control over PyQt and PyQwt widgets from the command line interpreter.
 """
 
-import sys
-
-# Import GNU readline or inputhooker; is also usable in Python scripts.
-try:
-    import readline
-    if '.py' in sys.modules['readline'].__file__:
-        # try inputhooker when readline is not GNU readline
-        try:
-            import inputhooker
-        except ImportError:
-            print 'Install InputHooker from http://pyqwt.sourceforge.net.'
-except ImportError:
-    try:
-        import inputhooker
-    except ImportError:
-        print 'Install InputHooker from http://pyqwt.sourceforge.net.'
-    
-from PyQt4.QtGui import QApplication
+from PyQt4.Qt import QApplication, PYQT_VERSION
 _a = QApplication([])
 
-import _iqt 
+if PYQT_VERSION < 0x40300:
+    import _iqt
 
 # Local Variables: ***
 # mode: python ***
