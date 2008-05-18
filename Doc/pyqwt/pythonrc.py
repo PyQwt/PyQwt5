fcf125150b9d9330edd2757fa9a7ba6c8cd1a4b0
Gerard Vermeulen <gvermeul@users.sourceforge.net>
2008-05-18 17:59:47 +0000

    Reorganize pythonrc.py such that iqt is superfluous.

--- Doc/pyqwt/pythonrc.py
+++ Doc/pyqwt/pythonrc.py
@@ -3,51 +3,48 @@
 # inspired by:
 # http://opag.ca/wiki/OpagCode/OpagSnippets
 
-from atexit import register
-from os import path
-import readline
-import rlcompleter
-
-# Sets up a tab for completion (use a single space to indent Python code).
-readline.parse_and_bind('tab: complete')
-
-historyPath = path.expanduser('~/.python_history')
-readline.set_history_length(1000)
-
-# Reads the history of the previous session, if it exists.
-if path.exists(historyPath):
-    readline.read_history_file(historyPath)
-
-# Sets up history saving on exit.
-def save_history(historyPath=historyPath, readline=readline):
-    readline.write_history_file(historyPath)
-
-register(save_history)
-
-# Cleans up the global name space.
-del register, path, readline, rlcompleter, historyPath, save_history
-
-# Tries to make the PyQt and PyQwt widgets usable from the command line.
-try:
-    import PyQt4.Qwt5.iqt
-    del PyQt4.Qwt5.iqt
-except ImportError:
-    pass
-
-# Tries to import PyQt and Qwt.
-try:
-    import PyQt4.Qwt5 as Qwt
-    import PyQt4.Qt as Qt
-except ImportError:
-    pass
-
-# Sets up the SciPy help for tab completion: help(sp.optimize.leastsq).
+# Import Numpy and SciPy
 try:
     import numpy as np
     import scipy as sp
     sp.pkgload()
 except ImportError:
     pass
+
+# Import PyQt4.Qt and PyQt4.Qwt5; initialize an application.
+# Note: hides the builtins hex and oct.
+try:
+    from PyQt4.Qt import *
+    from PyQt4.Qwt5 import *
+    application = QApplication([])
+except ImportError:
+    application = None
+
+# Setup readline and history saving
+from atexit import register
+from os import path
+import readline
+import rlcompleter
+
+# Set up a tab for completion; use a single space to indent Python code.
+readline.parse_and_bind('tab: complete')
+
+history = path.expanduser('~/.python_history')
+readline.set_history_length(1000)
+
+# Read the history of the previous session, if it exists.
+if path.exists(history):
+    readline.read_history_file(history)
+
+# Set up history saving on exit.
+def save(history=history, readline=readline, application=application):
+    readline.write_history_file(history)
+
+register(save)
+
+# Clean up the global name space; readline, appliction, and save will persist,
+# since del decrements the reference count by one.
+del register, path, readline, rlcompleter, history, save, application
 
 # Local Variables: ***
 # mode: python ***
